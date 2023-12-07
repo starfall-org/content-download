cbb570dcb6581f6f7b978843ccc483719afec08a
gitmakes <152869680+gitmakes@users.noreply.github.com>
2023-12-07 18:51:11 +0700

    update start.sh

--- start.sh
+++ start.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
 
 python main.py runserver &
-gunicorn --log-level critical --bind 0.0.0.0:8080 webpage.flask:app
+gunicorn --log-level critical --bind 0.0.0.0:8080 webpage.flask:app &
+
+sleep 10 
+
+while true; do
+  echo "Content Download"
+  sleep 169200
+done
