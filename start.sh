4ac948872338d0382ad7149b7b9a779b416e6ec9
gitmakes <152869680+gitmakes@users.noreply.github.com>
2023-12-07 18:53:27 +0700

    update start.sh

--- start.sh
+++ start.sh
@@ -6,6 +6,10 @@ gunicorn --log-level critical --bind 0.0.0.0:8080 webpage.flask:app &
 sleep 10 
 
 while true; do
-  echo "Content Download"
+  current_time=$(date -u +"%Y-%m-%d %H:%M:%S")
+  gmt7_time=$(TZ=Asia/Ho_Chi_Minh date +"%Y-%m-%d %H:%M:%S")
+  
+  echo "Content Download - UTC: $current_time, GMT+7: $gmt7_time"
+  
   sleep 169200
 done
