8496d2f6436d6e38b73bc846657f10d2b29bb487
gitmakes <152869680+gitmakes@users.noreply.github.com>
2023-12-08 18:19:29 +0700

    update plugins/BasicCommand.py

--- plugins/BasicCommand.py
+++ plugins/BasicCommand.py
@@ -2,8 +2,15 @@ from pyrogram import filters, Client
 from utils.functions import save
 from utils.variables import button
 from utils.variables import t
-from utils.basic_text import start_vn, start_en, help_vn, help_en
 
+start_vn = '''**Content Download bot** cho phép bạn tải nội dung từ nhiều nguồn khác nhau trên mạng xã hội. 
+Bạn có thể tải video, hình ảnh, âm thanh, hoặc tệp từ các trang web như Youtube, TikTok/Douyin, Facebook, Twitter, và nhiều trang web khác. 
+
+Dùng lệnh /help để biết thêm chi tiết.'''
+start_en = '''**Content Download bot** allows you to download content from various sources on social media. 
+You can download videos, images, audio, or file from websites such as Youtube, TikTok/Douyin, Facebook, Twitter, and many more. 
+
+Send /help command for more details.'''
 
 @Client.on_message(filters.command("start") & filters.private)
 def handle_start(c, m):
@@ -16,6 +23,18 @@ def handle_start(c, m):
     msg = start_en
   m.reply(msg, reply_markup=button)
 
+help_vn = ''' **Hướng dẫn:**
+
+`URL chia sẻ:` Tải nội dung từ URL
+`/music + URL:` Tải nội dung âm thanh từ URL
+`/upload:` Đăng tải tệp lên đám mây từ tin nhắn và nhận liên kết chia sẻ
+`/cloud:` Nền tảng lưu trữ'''
+help_en = '''**Instructions:**
+
+`Share URL:` Download content from URL
+`/music + URL:` Download music content from URL
+`/upload:` Upload file to cloud from message and get shareable link
+`/cloud:` Storage platform'''
 
 @Client.on_message(filters.command('help') & (filters.private | filters.mentioned))
 def handle_help(c, m):
