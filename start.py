8a74e16c1fc6c0eb05c571eb9d3aa5b813c6e504
gitmakes <152869680+gitmakes@users.noreply.github.com>
2023-12-18 18:57:24 +0700

    update start.py

--- start.py
+++ start.py
@@ -2,5 +2,5 @@ import sys, os
 from gunicorn.app.wsgiapp import run
 if __name__ == '__main__':
     os.system("python3 main.py runserver &")
-    sys.argv = "gunicorn --timeout 1200 --bind 0.0.0.0:3000 main:app".split()
+    sys.argv = "gunicorn --timeout 1200 --bind 0.0.0.0:3000 application.flask:app".split()
     sys.exit(run())
