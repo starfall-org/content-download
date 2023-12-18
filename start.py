be2e57605483133b39094ac3480a9363774464f9
gitmakes <152869680+gitmakes@users.noreply.github.com>
2023-12-18 19:09:23 +0700

    update start.py

--- start.py
+++ start.py
@@ -1,7 +1,7 @@
 import sys
 from gunicorn.app.wsgiapp import run
 from application.contentdownload import runserver
-from threading import Threads
+from threading import Thread
 def run_web():
   sys.argv = "gunicorn --timeout 1200 --bind 0.0.0.0:3000 application.flask:app".split()
   sys.exit(run())
