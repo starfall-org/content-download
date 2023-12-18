2b43b46dde196430fa033914f8eae2bf9239ff0d
gitmakes <152869680+gitmakes@users.noreply.github.com>
2023-12-18 18:57:11 +0700

    update start.py

---
+++ start.py
@@ -0,0 +1,6 @@
+import sys, os
+from gunicorn.app.wsgiapp import run
+if __name__ == '__main__':
+    os.system("python3 main.py runserver &")
+    sys.argv = "gunicorn --timeout 1200 --bind 0.0.0.0:3000 main:app".split()
+    sys.exit(run())
