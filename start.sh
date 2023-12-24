6854087268f831ebd6c68841f5ffd7e618f7d5d1
mymaking <152869680+mymaking@users.noreply.github.com>
2023-12-24 12:05:45 +0700

    update start.sh

--- start.sh
+++ start.sh
@@ -3,8 +3,7 @@
 python3 main.py runserver &
 gunicorn --log-level critical --bind 0.0.0.0:8080 application.flask:app &
 
-sleep 10 
-
+sleep 5
 while true; do
   gmt7_time=$(TZ=Asia/Ho_Chi_Minh date +"%Y-%m-%d %H:%M:%S")
   echo "$gmt7_time"
