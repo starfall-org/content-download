8cf3d52080afe9416dc502938fbb178ba0841de9
mymaking <152869680+mymaking@users.noreply.github.com>
2023-12-25 12:36:46 +0700

    update application/contentdownload.py

--- application/contentdownload.py
+++ application/contentdownload.py
@@ -5,8 +5,9 @@ app = pyrogram.Client("Content Download",
                       api_id=tokens()[0],
                       api_hash=tokens()[1],
                       bot_token=tokens()[2],
-                      plugins={"root": "plugins"},
-                      max_concurrent_transmissions=100)
+                      plugins = dict(root="plugins",
+                      exclude=["MediaGroup"]
+                      ), max_concurrent_transmissions=100)
 
 
 def runserver():
