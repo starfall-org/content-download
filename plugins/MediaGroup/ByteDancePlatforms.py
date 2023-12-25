cf18d454493635d7f19d63a63d2231f3cbb7c49a
mymaking <152869680+mymaking@users.noreply.github.com>
2023-12-25 10:17:48 +0700

    update plugins/MediaGroup/ByteDancePlatforms.py

--- plugins/MediaGroup/ByteDancePlatforms.py
+++ plugins/MediaGroup/ByteDancePlatforms.py
@@ -1,10 +1,53 @@
 from pyrogram import filters, Client
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 from api_callback.ByteDance import TDDL
-from etc.util import save, send_photos, uploads
+from etc.util import save, send_videos, send_photos, uploads, get_share_links
 from etc.var import sv, rv
 import re
 
+@Client.on_message(filters.regex(r"www.tiktok.com/@"))
+def handle_tiktokuserlink(c,m):
+  save(m)
+  text = m.text
+  url = re.search(r"(?P<url>https?://[^\s]+)", text).group("url")
+  original = InlineKeyboardMarkup([[InlineKeyboardButton("TikTok User",
+                                                         url=url)]])
+  m.reply_chat_action(rv)
+  try:
+    user_name = m.sender_chat.title
+    user_id = m.sender_chat.id
+  except:
+    user_name = m.from_user.first_name
+    user_id = m.from_user.id
+  caption = f'**[{user_name}](tg://user?id={user_id})**'
+  download = m.reply("**Downloading**`...`", quote=True)   
+  share_links = get_share_links(url)
+  list_video = []
+  list_file = []
+  list_photo
+  for sharelink in share_links:
+    file, link, is_video = TDDL(url)
+    if is_video == True:
+      list_video.append(link)
+      list_file.append(file)
+    else:
+      list_photo.extend(link)
+  c.delete_messages(m.chat.id, download.id)
+  sending = m.reply("**Sending**`...`", quote=True)
+  if list_photo is not None:
+    send_photos(m, c, original, list_photo, caption)
+  if list_video is not None:
+    try:
+      send_videos(m, c, original, list_video, caption)
+    except:
+      send_videos(m, c, original, list_file, caption)
+  c.delete_messages(m.chat.id, sending.id)
+  try:
+    m.delete()
+  except:
+    pass
+  
+  
 
 @Client.on_message(filters.regex(r"douyin.com|tiktok.com|iesdouyin.com"))
 def handle_tiktokdouyin(c, m):
@@ -24,18 +67,18 @@ def handle_tiktokdouyin(c, m):
   download = m.reply("**Downloading**`...`", quote=True)
   file, link, is_video = TDDL(url)
   c.delete_messages(m.chat.id, download.id)
+  sending = m.reply("**Sending**`...`", quote=True)
   if is_video == False:
     send_photos(m, c, original, file, caption)
   elif is_video == True:
-    sending = m.reply("**Sending**`...`", quote=True)
     m.reply_chat_action(sv)
     try:
       m.reply_video(link, reply_markup=original, caption=caption)
     except:
       m.reply_video(file, reply_markup=original, caption=caption)
-    c.delete_messages(m.chat.id, sending.id)
     if m.chat.username == "contentdownload":
       uploads(file)
+  c.delete_messages(m.chat.id, sending.id)
   try:
     m.delete()
   except:
