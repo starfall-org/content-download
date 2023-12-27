c7a20ff2937f706103d4ccc5f4d73e640ae47ce0
mymaking <152869680+mymaking@users.noreply.github.com>
2023-12-27 15:40:48 +0700

    update plugins/media/meta.py

--- plugins/media/meta.py
+++ plugins/media/meta.py
@@ -2,16 +2,27 @@ from api_callback.Meta import FBDL, IGDL
 from etc.util import send_videos, send_photos
 from etc.var import rv, sv
 
-def facebook(c, m, url, original, caption, download):
+def facebook(c, m, download, getattrs):
+  url, original, caption = getattrs(m=m)
   m.reply_chat_action(rv)
   files = FBDL(url)
   c.delete_messages(m.chat.id, download.id)
   m.reply_chat_action(sv)
   sending = m.reply("**Sending**`...`", quote=True)
-  send_videos(m, c, original, files, caption)
+  m.reply_chat_action(sv)
+  if not isinstance(files, list):
+    s = m.reply_video(files, reply_markup=original, caption=caption)
+  else:
+    for file in files:
+      s = m.reply_video(file, reply_markup=original, caption=caption)
+  original = getattrs(s=s)
+  c.edit_inline_reply_markup(s.id, reply_markup=original)
+      
+  #send_videos(m, c, original, files, caption)
   c.delete_messages(m.chat.id, sending.id)
 
-def instagram(c, m, url, original, caption, download):
+def instagram(c, m, download, getattrs):
+  url, original, caption = getattrs(m=m)
   m.reply_chat_action(rv)
   files, video = IGDL(url)
   c.delete_messages(m.chat.id, download.id)
