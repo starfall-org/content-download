6c183149f7bdc207af56d2fcab7de712b7421fa0
mymaking <152869680+mymaking@users.noreply.github.com>
2023-12-27 15:48:19 +0700

    update plugins/MediaDownload.py

--- plugins/MediaDownload.py
+++ plugins/MediaDownload.py
@@ -4,28 +4,15 @@ from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 from plugins.media.bytedance import tikdou
 from plugins.media.meta import facebook, instagram
 from plugins.media.youtube import youtube, music, other
+from api_callback.Youtube import ODL
 from etc.util import save
-from etc.var import t
-import re
-
-def getattrs(m):
-  url = re.search(r"(?P<url>https?://[^\s]+)", m.text).group("url")
-  original = InlineKeyboardMarkup([[InlineKeyboardButton("Original", url=url)]])
-  try:
-    user_name = m.sender_chat.title
-    user_id = m.sender_chat.id
-  except:
-    user_name = m.from_user.first_name
-    user_id = m.from_user.id
-  caption = f'**[{user_name}](tg://user?id={user_id})**'
-  return url, original, caption
+from etc.var import t, getattrs
 
 @Client.on_message(filters.command("music"))
 def music_handler(c, m):
   save(m)
-  url, _, caption = getattrs(m)
-  download = m.reply("**Downloading**`...`", quote=True)
-  music(c, m, url, caption, download)
+  d = m.reply("**Downloading**`...`", quote=True)
+  music(c, m, d, getattrs)
   try:
     m.delete()
   except:
@@ -33,25 +20,25 @@ def music_handler(c, m):
 
 @Client.on_message(filters.regex("https://|http://"))
 def media_handler(c, m):
-  url, original, caption= getattrs(m)
+  url, _, __ = getattrs(m=m)
   media_group = ["youtube", "youtu.be", "tiktok", "douyin", "iesdouyin", "facebook", "fb", "instagram"]
   is_media = False
   if any(media in url for media in media_group):
     m.reply_chat_action(t)
     save(m)
-    download = m.reply("**Downloading**`...`", quote=True)
+    d = m.reply("**Downloading**`...`", quote=True)
     if any(reg in url for reg in ["youtube", "youtu.be"]):
-      youtube(c, m, url, original, caption, download)
+      youtube(c, m, d, getattrs)
     elif any(reg in url for reg in ["facebook", "fb"]):
-      facebook(c, m, url, original, caption, download)
+      facebook(c, m, d, getattrs)
     elif "instagram" in url:
-      instagram(c, m, url, original, caption, download)
+      instagram(c, m, d, getattrs)
     else:
-      tikdou(c, m, url, original, caption, download)
+      tikdou(c, m, d, getattrs)
     is_media = True
   else:
     try:
-      file, type = ODL(url)
+      f, t = ODL(url)
       if file is None:
         return
     except Exception as e:
@@ -60,7 +47,7 @@ def media_handler(c, m):
     save(m)
     m.reply_chat_action(t)
     download = m.reply("**Downloading**`...`", quote=True)
-    other(c, m, file, type, original, caption, download)
+    other(c, m, f, t, d, getattrs)
     is_media = True
   if is_media:
     try:
