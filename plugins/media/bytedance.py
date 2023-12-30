e773dcb84ae132adfd75935bdad9e35f40e97a69
mymaking <152869680+mymaking@users.noreply.github.com>
2023-12-30 14:33:43 +0000

    update plugins/media/bytedance.py

--- plugins/media/bytedance.py
+++ plugins/media/bytedance.py
@@ -4,7 +4,7 @@ from etc.util import send_videos, send_photos, uploads, get_share_links
 from etc.var import sv, rv
 import logging
 
-def tiktokuserlink(c, m, url, caption, s):
+def tiktokuserlink(c, m, stt, url, caption):
   original = InlineKeyboardMarkup([[InlineKeyboardButton("TikTok User",
                                                          url=url)]])
   share_links = get_share_links(url)
@@ -18,7 +18,7 @@ def tiktokuserlink(c, m, url, caption, s):
       list_file.append(file)
     else:
       list_photo.extend(link)
-  s.edit("**Sending**`...`")
+  stt.edit("**Sending**`...`")
   if list_photo:
     try:
       send_photos(m, c, original, list_photo, caption)
@@ -30,24 +30,24 @@ def tiktokuserlink(c, m, url, caption, s):
     except:
       send_videos(m, c, original, list_file, caption)
   
-def tikdou(c, m, s, getattrs):
+def tikdou(c, m, stt, getattrs):
   url, original, caption = getattrs(m=m)
   m.reply_chat_action(rv)
   try:
     file, link, is_video = TDDL(url)
   except:
-    tiktokuserlink(c, m, url, caption, download)
+    tiktokuserlink(c, m, stt, url, caption)
     return
-  s.edit("**Sending**`...`")
+  stt.edit("**Sending**`...`")
   if is_video == False:
-    send_photos(m, c, original, file, caption)
+    send_photos(c, m, original, file, caption)
   elif is_video == True:
     m.reply_chat_action(sv)
     try:
-      st = m.reply_video(link, caption=caption, reply_markup=original)
+      sf = m.reply_video(link, caption=caption, reply_markup=original)
     except:
-      st = m.reply_video(file, caption=caption, reply_markup=original)
-    original = getattrs(m, st) 
-    st.edit_reply_markup(original)
+      sf = m.reply_video(file, caption=caption, reply_markup=original)
+    original = getattrs(m, sf) 
+    sf.edit_reply_markup(original)
     if m.chat.username == "contentdownload":
       uploads(file)
