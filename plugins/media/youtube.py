0b4bc53d8f1325e88749b04ac70f7c554bc68f18
mymaking <152869680+mymaking@users.noreply.github.com>
2023-12-30 13:43:28 +0000

    update plugins/media/youtube.py

--- plugins/media/youtube.py
+++ plugins/media/youtube.py
@@ -1,4 +1,3 @@
-from pyrogram.types import InputMediaVideo, InputMediaAudio, InputMediaPhoto, InputMediaDocument
 from api_callback.Youtube import YTDL,YTM, ODL
 from etc.var import t, rv, ra, sv, sp, sm
 
@@ -8,31 +7,27 @@ def youtube(c, m, s, getattrs):
   file = YTDL(url)
   m.reply_chat_action(sv)
   s.edit("**Sending**`...`")
-  s.edit_media(InputMediaVideo(file))
-  s.edit_caption(caption=caption, reply_markup=original)
-  original = getattrs(m, s)
-  s.edit_reply_markup(original)
+  st = m.reply_video(file, caption=caption, reply_markup=original)
+  original = getattrs(m, st)
+  st.edit_reply_markup(original)
 
 def other(c, m, file, type, s, getattrs):
   _, original, caption = getattrs(m=m)
   m.reply_chat_action(t)
   s.edit("**Sending**`...`")
-  s = None
+  st = None
   if type == "image":
     m.reply_chat_action(sp)
-    s.edit_media(InputMediaPhoto(file))
-    s.edit_caption(caption=caption, reply_markup=original)
+    st = m.reply_photo(file, caption=caption, reply_markup=original)
   elif type == "video":
     m.reply_chat_action(sv)
-    s.edit_media(InputMediaVideo(file))
-    s.edit_caption(caption=caption, reply_markup=original)
+    st = m.reply_video(file, caption=caption, reply_markup=original)
   elif type == "audio":
     m.reply_chat_action(sm)
-    s.edit_media(InputMediaAudio(file), caption=caption)
-  else:
-    return
-  original = getattrs(m, s)
-  s.edit_reply_markup(original)
+    m.reply_audio(file, caption=caption)
+  if st:
+    original = getattrs(m, st)
+    st.edit_reply_markup(original)
 
 def music(c, m, s, getattrs):
   url, _, caption = getattrs(m=m)
@@ -40,4 +35,4 @@ def music(c, m, s, getattrs):
   audio = YTM(url)
   s.edit("**Sending**`...`")
   m.reply_chat_action(sm)
-  s.edit_media(InputMediaAudio(audio), caption=caption)
+  m.reply_audio(audio, caption=caption)
