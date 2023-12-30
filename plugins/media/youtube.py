49b50ac7ae1f9b285064962fdb9fba23b183bbc4
mymaking <152869680+mymaking@users.noreply.github.com>
2023-12-30 14:33:40 +0000

    update plugins/media/youtube.py

--- plugins/media/youtube.py
+++ plugins/media/youtube.py
@@ -1,38 +1,38 @@
 from api_callback.Youtube import YTDL,YTM, ODL
 from etc.var import t, rv, ra, sv, sp, sm
 
-def youtube(c, m, s, getattrs):
+def youtube(c, m, stt, getattrs):
   url, original, caption = getattrs(m=m)
   m.reply_chat_action(rv)
   file = YTDL(url)
   m.reply_chat_action(sv)
-  s.edit("**Sending**`...`")
-  st = m.reply_video(file, caption=caption, reply_markup=original)
-  original = getattrs(m, st)
-  st.edit_reply_markup(original)
+  stt.edit("**Sending**`...`")
+  sf = m.reply_video(file, caption=caption, reply_markup=original)
+  original = getattrs(m, sf)
+  sf.edit_reply_markup(original)
 
-def other(c, m, file, type, s, getattrs):
+def other(c, m, file, tp, stt, getattrs):
   _, original, caption = getattrs(m=m)
   m.reply_chat_action(t)
-  s.edit("**Sending**`...`")
-  st = None
+  stt.edit("**Sending**`...`")
+  sf = None
   if type == "image":
     m.reply_chat_action(sp)
-    st = m.reply_photo(file, caption=caption, reply_markup=original)
+    sf = m.reply_photo(file, caption=caption, reply_markup=original)
   elif type == "video":
     m.reply_chat_action(sv)
-    st = m.reply_video(file, caption=caption, reply_markup=original)
+    sf = m.reply_video(file, caption=caption, reply_markup=original)
   elif type == "audio":
     m.reply_chat_action(sm)
     m.reply_audio(file, caption=caption)
-  if st:
-    original = getattrs(m, st)
-    st.edit_reply_markup(original)
+  if sf:
+    original = getattrs(m, sf)
+    sf.edit_reply_markup(original)
 
-def music(c, m, s, getattrs):
+def music(c, m, stt, getattrs):
   url, _, caption = getattrs(m=m)
   m.reply_chat_action(ra)
   audio = YTM(url)
-  s.edit("**Sending**`...`")
+  stt.edit("**Sending**`...`")
   m.reply_chat_action(sm)
   m.reply_audio(audio, caption=caption)
