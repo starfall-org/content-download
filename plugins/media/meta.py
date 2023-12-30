ed399f29d9c9e82e35f4d499b53d3d2e074eadfb
mymaking <152869680+mymaking@users.noreply.github.com>
2023-12-30 14:33:36 +0000

    update plugins/media/meta.py

--- plugins/media/meta.py
+++ plugins/media/meta.py
@@ -3,28 +3,28 @@ from api_callback.Meta import FBDL, IGDL
 from etc.util import send_videos, send_photos
 from etc.var import rv, sv
 
-def facebook(c, m, s, getattrs):
+def facebook(c, m, stt, getattrs):
   url, original, caption = getattrs(m=m)
   m.reply_chat_action(rv)
   files = FBDL(url)
   m.reply_chat_action(sv)
-  s.edit("**Sending**`...`")
+  stt.edit("**Sending**`...`")
   m.reply_chat_action(sv)
   if not isinstance(files, list):
-    st = m.reply_video(files, caption=caption, reply_markup=original)
+    sf = m.reply_video(files, caption=caption, reply_markup=original)
   else:
     for file in files:
-      st = m.reply_video(file, caption=caption, reply_markup=original)
-  original = getattrs(m, st)
-  st.edit_reply_markup(original)
-  #send_videos(m, c, original, files, caption)
+      sf = m.reply_video(file, caption=caption, reply_markup=original)
+  original = getattrs(m, sf)
+  sf.edit_reply_markup(original)
+  #send_videos(c, m, original, files, caption)
 
-def instagram(c, m, s, getattrs):
+def instagram(c, m, stt, getattrs):
   url, original, caption = getattrs(m=m)
   m.reply_chat_action(rv)
   files, video = IGDL(url)
-  s.edit("**Sending**`...`")
+  stt.edit("**Sending**`...`")
   if video == False:
-    send_photos(m, c, original, files, caption)
+    send_photos(c, m, original, files, caption)
   elif video == True:
-    send_videos(m, c, original, files, caption)
+    send_videos(c, m, original, files, caption)
