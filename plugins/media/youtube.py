57f56827ff693cdf458215088211f382ad6e950c
mymaking <152869680+mymaking@users.noreply.github.com>
2023-12-30 13:08:45 +0000

    update plugins/media/youtube.py

--- plugins/media/youtube.py
+++ plugins/media/youtube.py
@@ -1,45 +1,43 @@
+from pyrogram.types import InputMediaVideo, InputMediaAudio, InputMediaPhoto, InputMediaDocument
 from api_callback.Youtube import YTDL,YTM, ODL
 from etc.var import t, rv, ra, sv, sp, sm
 
-def youtube(c, m, status, getattrs):
+def youtube(c, m, s, getattrs):
   url, original, caption = getattrs(m=m)
   m.reply_chat_action(rv)
   file = YTDL(url)
   m.reply_chat_action(sv)
-  status.edit("**Sending**`...`")
-  s = m.reply_video(file, reply_markup=original, caption=caption)
+  s.edit("**Sending**`...`")
+  s.edit_media(InputMediaVideo(file))
+  s.edit_caption(caption=caption, reply_markup=original)
   original = getattrs(m, s)
   s.edit_reply_markup(original)
-  status.delete()
-  try:
-    m.delete()
-  except:
-    pass
 
-def other(c, m, file, type, status, getattrs):
+def other(c, m, file, type, s, getattrs):
   _, original, caption = getattrs(m=m)
   m.reply_chat_action(t)
-  status.edit("**Sending**`...`")
+  s.edit("**Sending**`...`")
   s = None
   if type == "image":
     m.reply_chat_action(sp)
-    s = m.reply_photo(file, reply_markup=original, caption=caption)
+    s.edit_media(InputMediaPhoto(file))
+    s.edit_caption(caption=caption, reply_markup=original)
   elif type == "video":
     m.reply_chat_action(sv)
-    s = m.reply_video(file, reply_markup=original, caption=caption)
+    s.edit_media(InputMediaVideo(file))
+    s.edit_caption(caption=caption, reply_markup=original)
   elif type == "audio":
     m.reply_chat_action(sm)
-    m.reply_audio(file, caption=caption)
-  if s:
-    original = getattrs(m, s)
-    s.edit_reply_markup(original)
-  status.delete()
+    s.edit_media(InputMediaAudio(file), caption=caption)
+  else:
+    return
+  original = getattrs(m, s)
+  s.edit_reply_markup(original)
 
-def music(c, m, status, getattrs):
+def music(c, m, s, getattrs):
   url, _, caption = getattrs(m=m)
   m.reply_chat_action(ra)
   audio = YTM(url)
-  status.edit("**Sending**`...`")
+  s.edit("**Sending**`...`")
   m.reply_chat_action(sm)
-  m.reply_audio(audio, caption=caption)
-  status.delete()
+  s.edit_media(InputMediaAudio(audio), caption=caption)
