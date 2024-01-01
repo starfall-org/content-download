d4ef93e8c94bb85985cdf2ccb4de5f0be4779248
mymaking <152869680+mymaking@users.noreply.github.com>
2024-01-01 20:07:35 +0700

    update plugins/media/meta.py

--- plugins/media/meta.py
+++ plugins/media/meta.py
@@ -3,12 +3,10 @@ from api_callback.Meta import FBDL, IGDL
 from etc.util import send_videos, send_photos
 from etc.var import rv, sv
 
-def facebook(c, m, stt, getattrs):
+def facebook(c, m, getattrs):
   url, original, caption = getattrs(m=m)
   m.reply_chat_action(rv)
   files = FBDL(url)
-  m.reply_chat_action(sv)
-  stt.edit("**Sending**`...`")
   m.reply_chat_action(sv)
   if not isinstance(files, list):
     sf = m.reply_video(files, caption=caption, reply_markup=original)
@@ -17,13 +15,11 @@ def facebook(c, m, stt, getattrs):
       sf = m.reply_video(file, caption=caption, reply_markup=original)
   original = getattrs(m, sf)
   sf.edit_reply_markup(original)
-  #send_videos(m, files, original, caption)
 
-def instagram(c, m, stt, getattrs):
+def instagram(c, m, getattrs):
   url, original, caption = getattrs(m=m)
   m.reply_chat_action(rv)
   files, video = IGDL(url)
-  stt.edit("**Sending**`...`")
   if video == False:
     send_photos(m, files, original, caption)
   elif video == True:
