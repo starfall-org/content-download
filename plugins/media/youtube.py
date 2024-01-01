f58783cbc55c49410055e114c447d6152e8fa57e
mymaking <152869680+mymaking@users.noreply.github.com>
2024-01-01 20:07:32 +0700

    update plugins/media/youtube.py

--- plugins/media/youtube.py
+++ plugins/media/youtube.py
@@ -1,20 +1,17 @@
 from api_callback.Youtube import YTDL,YTM, ODL
 from etc.var import t, rv, ra, sv, sp, sm
 
-def youtube(c, m, stt, getattrs):
+def youtube(c, m, getattrs):
   url, original, caption = getattrs(m=m)
   m.reply_chat_action(rv)
   file = YTDL(url)
   m.reply_chat_action(sv)
-  stt.edit("**Sending**`...`")
   sf = m.reply_video(file, caption=caption, reply_markup=original)
   original = getattrs(m, sf)
   sf.edit_reply_markup(original)
 
-def other(c, m, file, tp, stt, getattrs):
+def other(c, m, file, tp, getattrs):
   _, original, caption = getattrs(m=m)
-  m.reply_chat_action(t)
-  stt.edit("**Sending**`...`")
   sf = None
   if type == "image":
     m.reply_chat_action(sp)
@@ -29,10 +26,9 @@ def other(c, m, file, tp, stt, getattrs):
     original = getattrs(m, sf)
     sf.edit_reply_markup(original)
 
-def music(c, m, stt, getattrs):
+def music(c, m, getattrs):
   url, _, caption = getattrs(m=m)
   m.reply_chat_action(ra)
   audio = YTM(url)
-  stt.edit("**Sending**`...`")
   m.reply_chat_action(sm)
   m.reply_audio(audio, caption=caption)
