62bb12d933e560fcc4d4be1330d885b4ce74a11d
mymaking <152869680+mymaking@users.noreply.github.com>
2024-01-01 21:07:41 +0700

    update plugins/MediaDownload.py

--- plugins/MediaDownload.py
+++ plugins/MediaDownload.py
@@ -18,7 +18,7 @@ def music_handler(c, m):
   except:
     pass
 
-@Client.on_message(filters.regex("https://|http://"))
+@Client.on_message(filters.regex("https://|http://") & filters.incoming)
 def media_handler(c, m):
   url, _, __ = getattrs(m=m)
   media_group = ["youtube", "youtu.be", "tiktok", "douyin", "iesdouyin", "facebook", "fb", "instagram"]
