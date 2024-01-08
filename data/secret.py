5f65551722aaeeba3e75a0ae648595ca8c545a27
mymaking <152869680+mymaking@users.noreply.github.com>
2024-01-08 14:40:51 +0700

    update data/secret.py

--- data/secret.py
+++ data/secret.py
@@ -1,14 +1,14 @@
 import os
 from deta import Deta
 
-upload_app = os.getenv("UPLOAD_APP")
-api_url = os.getenv("API_URL")
+webapi = os.getenv("WEBAPI")
 webstream = os.getenv("WEBSTREAM")
+collection = os.getenv("COLLECTION")
 deta = Deta(os.getenv('DETA_KEY'))
-db = deta.Base('telegram-sessions')
 
 def tokens():
-  api_id = db.get('API_ID')
-  api_hash = db.get('API_HASH')
-  bot_token = db.get('CD_TOKEN')
-  return api_id['value'], api_hash['value'], bot_token['value']
+    db = deta.Base('telegram-sessions')
+    api_id = db.get('API_ID')
+    api_hash = db.get('API_HASH')
+    bot_token = db.get('CD_TOKEN')
+    return api_id['value'], api_hash['value'], bot_token['value']
