af8ec9d5e6bded82439e247d2063b93f6b1a0fae
mymaking <152869680+mymaking@users.noreply.github.com>
2024-01-08 15:07:04 +0700

    update media/youtube.py

--- media/youtube.py
+++ media/youtube.py
@@ -6,25 +6,25 @@ def youtube(c, m, getattrs):
   m.reply_chat_action(rv)
   file = YTDL(url)
   m.reply_chat_action(sv)
-  sf = m.reply_video(file, caption=caption, reply_markup=original)
-  original = getattrs(m, sf)
-  sf.edit_reply_markup(original)
+  st = m.reply_video(file, caption=caption, reply_markup=original)
+  original = getattrs(m, st)
+  st.edit_reply_markup(original)
 
 def other(c, m, file, tp, getattrs):
   _, original, caption = getattrs(m=m)
-  sf = None
+  st = None
   if type == "image":
     m.reply_chat_action(sp)
-    sf = m.reply_photo(file, caption=caption, reply_markup=original)
+    st = m.reply_photo(file, caption=caption, reply_markup=original)
   elif type == "video":
     m.reply_chat_action(sv)
-    sf = m.reply_video(file, caption=caption, reply_markup=original)
+    st = m.reply_video(file, caption=caption, reply_markup=original)
   elif type == "audio":
     m.reply_chat_action(sm)
     m.reply_audio(file, caption=caption)
-  if sf:
-    original = getattrs(m, sf)
-    sf.edit_reply_markup(original)
+  if st:
+    original = getattrs(m, st)
+    st.edit_reply_markup(original)
 
 def music(c, m, getattrs):
   url, _, caption = getattrs(m=m)
