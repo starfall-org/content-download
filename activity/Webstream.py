ae9d65b681fb4c33af575f309e357ec6b58a4412
mymaking <152869680+mymaking@users.noreply.github.com>
2024-01-08 14:50:07 +0700

    update activity/Webstream.py

--- activity/Webstream.py
+++ activity/Webstream.py
@@ -1,9 +1,9 @@
 from pyrogram import Client, filters
+from data.secret import webstream
 import os 
 
-@Client.on_message(filters.command('stream'), group=2)
+@Client.on_message(filters.command('stream'))
 def stream(c,m):
-  stream_url = os.getenv('WEB_STREAM')
   if m.reply_to_message:
     reply_to = m.reply_to_message
     box = reply_to.chat.id
@@ -32,11 +32,11 @@ def stream(c,m):
     else:
       m.reply('Không phát hiện đối tượng có thể stream, vui lòng cung cấp thêm.', quote=True)
       return
-  main_stream = f'{stream_url}/stream?box={box}&id={mid}'
+  main_stream = f'{webstream}/stream?box={box}&id={mid}'
   m.reply(f'Liên kết Stream:\n{main_stream}', quote=True)
   
-@Client.on_message(filters.command('setstream'), group=2)
-def set_stream_url(c,m):
+@Client.on_message(filters.command('setstream'))
+def set_webstream(c,m):
   url = m.command[1]
-  os.environ['WEB_STREAM'] = url
+  os.environ['WEBSTREAM'] = url
   m.reply(f'Đã cập nhật stream url thành {url}', quote=True)
