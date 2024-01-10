50a03229d69af8dd95ca2e3890cf4aef65848216
mymaking <152869680+mymaking@users.noreply.github.com>
2024-01-10 12:50:14 +0700

    update api/bytedance.py

--- api/bytedance.py
+++ api/bytedance.py
@@ -1,19 +1,25 @@
-import requests, random, logging
+import requests, logging
 from data.secret import dapi
 from io import BytesIO
 
 
 def TDDL(url):
-  data = requests.get(f"{dapi}/tiktokdouyin",
+  data = requests.get(f"{dapi}/tikdou",
                       params={
                           "url": url
                       },
                       timeout=60).json()
-  if data['video'] == True:
+  music = None
+  if data['is_video'] == True:
     content = requests.get(data["url"]).content
     file = BytesIO(content)
-    file.name = f"file{random.randint(1, 9999)}.mp4"
+    file.name = "video.mp4"
   else:
     file = data["url"]
+    music_url = data.get("music")
+    if music_url:
+        music_data = requests.get(music_url).content
+        music = BytesIO(content)
+        music.name = "music.mp3"
   logging.critical("TikTok/Douyin")
-  return file, data["url"], data["video"]
+  return file, data["url"], data["is_video"], music
