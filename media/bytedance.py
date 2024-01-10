fc360e8234d022e5a34c594d106f581a5514723e
mymaking <152869680+mymaking@users.noreply.github.com>
2024-01-10 12:57:12 +0700

    update media/bytedance.py

--- media/bytedance.py
+++ media/bytedance.py
@@ -12,13 +12,16 @@ def tiktokuserlink(c, m, url, caption):
   list_video = []
   list_file = []
   list_photo = []
+  list_music = []
   for sharelink in share_links:
-    file, link, is_video = TDDL(sharelink)
+    file, link, is_video, music = TDDL(sharelink)
     if is_video == True:
       list_video.append(link)
       list_file.append(file)
     else:
       list_photo.extend(link)
+    if music:
+      list_music.append(music)
   if list_photo:
     try:
       send_photos(m, list_photo, original, caption)
@@ -29,17 +32,21 @@ def tiktokuserlink(c, m, url, caption):
       send_videos(m, list_video, original, caption)
     except:
       send_videos(m, list_file, original, caption)
+  if list_music:
+      send_audios(m, list_music, caption)
   
 def tikdou(c, m, getattrs):
   url, original, caption = getattrs(m=m)
   m.reply_chat_action(rv)
   try:
-    file, link, is_video = TDDL(url)
+    file, link, is_video, music = TDDL(url)
   except:
     tiktokuserlink(c, m, url, caption)
     return
   if is_video == False:
     send_photos(m, link, original, caption)
+    if music:
+        m.reply_audio(music, caption=caption)
   elif is_video == True:
     m.reply_chat_action(sv)
     try:
