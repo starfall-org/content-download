949aebd0233a081b65fcbdf4a4af24734ffafc7c
mymaking <152869680+mymaking@users.noreply.github.com>
2024-01-11 08:20:50 +0700

    update ext/upload.py

--- ext/upload.py
+++ ext/upload.py
@@ -5,18 +5,15 @@ from pytz import timezone
 from data.secret import collection
 from threading import Thread
 
-def upload(file):
-  uf = Thread(target=upload_file, args=(file, ))
-  uf.start()
+def upload(file_data):
+    current_time = datetime.now(timezone('Asia/Ho_Chi_Minh'))
+    formatted_time = current_time.strftime("%H:%M:%S(%d-%B-%Y)")
+    file_name = f"video {formatted_time}.mp4"
+    Thread(target=upload_file, args=(file_data, file_name,)).start()
 
-def upload_file(file_data):
+def upload_file(file_data, file_name):
   raw_bytes = file_data.getvalue()
-  current_time = datetime.now(timezone('Asia/Ho_Chi_Minh'))
-  formatted_time = current_time.strftime("%H:%M:%S(%d-%B-%Y)")
-
-  file_name = f"video {formatted_time}.mp4"
   headers = {'Content-type': 'video/mp4'}
-
   response = requests.put(f"{collection}/{file_name}",
                           data=raw_bytes,
                           headers=headers)
