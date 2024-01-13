fcd3dc3ee6febcf45328b2be826d3caa3c63ddd9
mymaking <152869680+mymaking@users.noreply.github.com>
2024-01-13 18:15:45 +0700

    update activity/Media.py

--- activity/Media.py
+++ activity/Media.py
@@ -12,8 +12,9 @@ import logging
 @Client.on_message(filters.command("music"))
 def music_handler(c, m):
   save(m)
-  url, _, __ = getattrs(m=m)
-  if not url:
+  try:
+      getattrs(m=m)
+  except:
       m.reply("Không tìm thấy liên kết", quote=True)
       return
   if any(match in m.text for match in ["tiktok", "douyin"]):
