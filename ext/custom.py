c8b65ee4529ed0161908527dea1d3609f381a3e8
mymaking <152869680+mymaking@users.noreply.github.com>
2024-01-17 00:20:41 +0700

    update ext/custom.py

--- ext/custom.py
+++ ext/custom.py
@@ -3,5 +3,12 @@ from pyrogram import filters
 def filter_owner(_, __, m):
     return m.from_user.id == 5665225938
     
+def content_download_channel(_, __, m):
+    if m.chat.username == "contentdownload_group":
+        if hasattr(m, "sender_chat"):
+            return m.sender_chat.username == "contentdownload":
+                return False
+    
 owner = filters.create(filter_owner)
+channel_post  = filters.create(content_download_channel)
     
