56640137bc2eb7a31c1fcf7fd8c363df8730cd05
mymaking <152869680+mymaking@users.noreply.github.com>
2024-01-16 23:23:02 +0700

    update application/content.py

--- application/content.py
+++ application/content.py
@@ -2,15 +2,15 @@ from data.secret import tokens
 from pyrogram import Client, idle
 import os
 
-client = Client("Content Download",
-    api_id=tokens()[0],
-    api_hash=tokens()[1],
-    bot_token=tokens()[2],
-    plugins=dict(root="activity"),
-    max_concurrent_transmissions=100)
-
-
-def run():
-  client.start()
-  os.system("echo Content Download")
-  idle()
+class Content(Client):
+    def __init__(self):
+        self.client = Client("Content Download",
+        api_id=tokens()[0],
+        api_hash=tokens()[1],
+        bot_token=tokens()[2],
+        plugins=dict(root="activity"),
+        max_concurrent_transmissions=100)
+    def run(self):
+        self.client.start()
+        os.system("echo Content Download")
+        idle()
