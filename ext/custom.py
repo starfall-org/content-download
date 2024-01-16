998586f98c3ebca3c7ecbc25a7af7d5a29e6f473
mymaking <152869680+mymaking@users.noreply.github.com>
2024-01-17 00:38:05 +0700

    update ext/custom.py

--- ext/custom.py
+++ ext/custom.py
@@ -4,10 +4,7 @@ def filter_owner(_, __, m):
     return m.from_user.id == 5665225938
     
 def content_download_channel(_, __, m):
-    if m.chat.username == "contentdownload_group":
-        if hasattr(m, "sender_chat"):
-            if m.sender_chat.username == "contentdownload":
-                return False
+    return m.chat.username == "contentdownload_group" and m.sender_chat.username != "contentdownload"
     
 owner = filters.create(filter_owner)
 channel_post  = filters.create(content_download_channel)
