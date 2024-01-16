7ee98ed508a47696e28d908a99bb3af633bee617
mymaking <152869680+mymaking@users.noreply.github.com>
2024-01-17 01:06:48 +0700

    update media/youtube.py

--- media/youtube.py
+++ media/youtube.py
@@ -2,30 +2,30 @@ from api import YTDL,YTM, ODL
 from ext.var import t, rv, ra, sv, sp, sm, sd
 
 def youtube(c, m, getattrs):
-  url, original, caption = getattrs(m)
-  m.reply_chat_action(rv)
-  file = YTDL(url)
-  m.reply_chat_action(sv)
-  m.reply_video(file, caption=caption, reply_markup=original)
-  
-def other(c, m, file, tp, getattrs):
-  _, original, caption = getattrs(m)
-  if type == "image":
-    m.reply_chat_action(sp)
-    m.reply_photo(file, caption=caption, reply_markup=original)
-  elif type == "video":
+    url, original, caption = getattrs(m)
+    m.reply_chat_action(rv)
+    file = YTDL(url)
     m.reply_chat_action(sv)
     m.reply_video(file, caption=caption, reply_markup=original)
-  elif type == "audio":
-    m.reply_chat_action(sm)
-    m.reply_audio(file, caption=caption)
-  else:
-    m.reply_chat_action(sd)
-    m.reply_document(file, reply_markup=original, caption=caption)
+    
+def other(c, m, file, tp, getattrs):
+    _, original, caption = getattrs(m)
+    if type == "image":
+        m.reply_chat_action(sp)
+        m.reply_photo(file, caption=caption, reply_markup=original)
+    elif type == "video":
+        m.reply_chat_action(sv)
+        m.reply_video(file, caption=caption, reply_markup=original)
+    elif type == "audio":
+        m.reply_chat_action(sm)
+        m.reply_audio(file, caption=caption)
+    else:
+        m.reply_chat_action(sd)
+        m.reply_document(file, reply_markup=original, caption=caption)
 
 def music(c, m, getattrs):
-  url, _, caption = getattrs(m)
-  m.reply_chat_action(ra)
-  audio = YTM(url)
-  m.reply_chat_action(sm)
-  m.reply_audio(audio, caption=caption)
+    url, _, caption = getattrs(m)
+    m.reply_chat_action(ra)
+    audio = YTM(url)
+    m.reply_chat_action(sm)
+    m.reply_audio(audio, caption=caption)
