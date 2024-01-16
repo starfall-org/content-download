8b2eed5174833a6bc2adc7e1f71e6876e88a7af2
mymaking <152869680+mymaking@users.noreply.github.com>
2024-01-17 01:04:19 +0700

    update media/meta.py

--- media/meta.py
+++ media/meta.py
@@ -4,20 +4,18 @@ from ext.util import send_videos, send_photos
 from ext.var import rv, sv
 
 def facebook(c, m, getattrs):
-  url, original, caption = getattrs(m=m)
+  url, original, caption = getattrs(m)
   m.reply_chat_action(rv)
   files = FBDL(url)
   m.reply_chat_action(sv)
   if not isinstance(files, list):
-    st = m.reply_video(files, caption=caption, reply_markup=original)
+     m.reply_video(files, caption=caption, reply_markup=original)
   else:
     for file in files:
-      st = m.reply_video(file, caption=caption, reply_markup=original)
-  original = getattrs(m, st)
-  st.edit_reply_markup(original)
+       m.reply_video(file, caption=caption, reply_markup=original)
 
 def instagram(c, m, getattrs):
-  url, original, caption = getattrs(m=m)
+  url, original, caption = getattrs(m)
   m.reply_chat_action(rv)
   files, video = IGDL(url)
   if video == False:
