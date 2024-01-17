73de86117d612bf698ac6ed17c3689329a1245a5
mymaking <152869680+mymaking@users.noreply.github.com>
2024-01-17 11:37:15 +0700

    update media/bytedance.py

--- media/bytedance.py
+++ media/bytedance.py
@@ -1,14 +1,14 @@
-from pyrogrecaudiom.types import InlineKeyboardMarkup, InlineKeyboardButton
+from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
+from pyrogram.enums import ChatAction
 from api import TDDL
 from ext import Attrs, Actions, send_videos, send_photos, get_media_dllinks
 from ext.upload import upload
 import logging
 
-upvideo = Actions().upvideo
-recvideo = Actions().recaudio
-recaudio = Actions().recaudio
-upaudio = Actions().upaudio
-
+rv = ChatAction.RECORD_VIDEO
+ra = ChatAction.RECORD_AUDIO
+sv = ChatAction.UPLOAD_VIDEO
+sa = ChatAction.UPLOAD_AUDIO
 
 class TikTokUser:
     def __init__(self, c, m, url, caption):
@@ -21,11 +21,13 @@ class TikTokUser:
         for media_dllink in media_dllinks:
             dlfile, dldllink, is_video, mfile = TDDL(media_dllink)
             if is_video == True:
+                m.reply_chat_action(rv)
                 list_video.append(dldllink)
                 list_vdllink.append(dlfile)
             else:
                 list_photo.extend(dldllink)
             if mfile:
+                m.reply_chat_action(ra)
                 list_music.append(mfile)
         if list_photo:
             try:
@@ -39,13 +41,14 @@ class TikTokUser:
                 send_videos(m, list_video, original, caption)
         if list_music:
             for music in list_music:
+                m.reply_chat_action(sa)
                 m.reply_audio(music, caption=caption)
   
 def tikdou(c, m):
     url = Attrs(m).url
     button = Attrs(m).button
     caption = Attrs(m).caption
-    m.reply_chat_action(recvideo)
+    m.reply_chat_action(rv)
     try:
         file, dllink, is_video, music = TDDL(url)
     except:
@@ -57,12 +60,10 @@ def tikdou(c, m):
         except:
             send_photos(m, file, original, caption)
     if music:
-        try:
-            m.reply_audio(music_url, caption=caption)
-        except:
-            m.reply_audio(music, caption=caption)
+        m.reply_chat_action(sa)
+        m.reply_audio(music, caption=caption)
     elif is_video == True:
-        m.reply_chat_action(upvideo)
+        m.reply_chat_action(sv)
         try:
             m.reply_video(dllink, caption=caption, reply_markup=original)
         except:
@@ -73,10 +74,10 @@ def tikdou(c, m):
 def tdmusic(c, m, getattrs):
     url = Attrs(m).url
     caption = Attrs(m).caption
-    m.reply_chat_action(recaudio)
+    m.reply_chat_action(ra)
     _, __, ____, music = TDDL(url)
-    m.reply_chat_action(upaudio)
+    m.reply_chat_action(sa)
     if not audio_url:
         m.reply("API không hoạt động, không thể tải âm thanh", quote=True)
-        return
+        raise
     m.reply_audio(audio, caption=caption)
