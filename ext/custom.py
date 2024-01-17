c7060266e4b73a5aee043ece5c7a1c0a2bbaae0c
mymaking <152869680+mymaking@users.noreply.github.com>
2024-01-17 20:27:17 +0700

    update ext/custom.py

--- ext/custom.py
+++ ext/custom.py
@@ -6,6 +6,10 @@ def filter_owner(_, __, m):
 def content_download_channel(_, __, m):
     return not m.sender_chat.username == "contentdownload" if m.chat.username == "contentdownload_group" else True
     
+def filter_media_link(_, __, m):
+    return if any(scheme in m.text for scheme in ["http://", "https://"]) else False
+    
 owner = filters.create(filter_owner)
 channel_post  = filters.create(content_download_channel)
+filter_media = filters.create(filter_media_link)
     
