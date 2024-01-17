5fac60638100636668bab4a4f4aebb8fe44a1850
mymaking <152869680+mymaking@users.noreply.github.com>
2024-01-17 09:44:39 +0700

    update api/bytedance.py

--- api/bytedance.py
+++ api/bytedance.py
@@ -4,32 +4,24 @@ from data.secret import dapi
 
 class TDDL:
     def __init__(self, url):
-        self.data = requests.get(f"{dapi}/tikdou", params={"url": url}, timeout=60).json()
-    def get(self):
+        data = requests.get(f"{dapi}/tikdou", params={"url": url}, timeout=60).json()
         music = None
-        if self.data['is_video'] == True:
+        if data['is_video'] == True:
             content = requests.get(data["url"]).content
             file = BytesIO(content)
             file.name = "tiktokdouyin.mp4"
             is_video = True
         else:
             file = []
-            for photo_url in self.data["url"]:
+            for photo_url in data["url"]:
                 photo_data = requests.get(photo_url).content
                 photo_file = BytesIO(photo_data)
                 photo_file.name = "photo.jpg"
                 file.append(photo_file)
             is_video = False
-        if self.data["music"]:
+        if data["music"]:
             music_data = requests.get(data["music"]).content
             music = BytesIO(music_data)
             music.name = "music.mp3"
         os.system("echo TikTok/Douyin")
-        return file, self.data["url"], is_video, music, self.data["music"]
-    def music(self):
-        if self.data["music"]:
-            music_data = requests.get(data["music"]).content
-            music = BytesIO(music_data)
-            music.name = "music.mp3"
-            os.system("echo TikTok Music")
-            return music, self.data["music"]
+        return file, data["url"], is_video, music, data["music"]
