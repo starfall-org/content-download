90c05494aa28312f6b3d1c97bcf7bb9bbbfa0543
mymaking <152869680+mymaking@users.noreply.github.com>
2024-01-17 18:00:45 +0700

    update activity/Update.py

---
+++ activity/Update.py
@@ -0,0 +1,22 @@
+from pyrogram import Client, filters
+from pyrogram.enums import ChatAction
+from ext import owner
+import subprocess
+import sys
+import os 
+
+typing = ChatAction.TYPING
+
+@Client.on_message(filters.command("update") & filters.private & owner)
+def system_update(c, m):
+    m.reply_chat_action(typing)
+    result = subprocess.run(["bash", "update.sh"], stdout=subprocess.PIPE, text=True)
+    m.reply(str(result.stdout))
+    os.execl(sys.executable, sys.executable, *sys.argv)
+    
+@Client.on_message(filters.command("shell") & owner)
+def run_shell(c, m):
+    m.reply_chat_action(typing)
+    command = m.text.replace("/shell ", "")
+    result = subprocess.run(command, stdout=subprocess.PIPE, text=True)
+    m.reply(str(result.stdout))
