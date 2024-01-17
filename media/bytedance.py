cf48d7490d36d8f7807c1d4f07a4b7ebc5b40d52
mymaking <152869680+mymaking@users.noreply.github.com>
2024-01-17 15:49:13 +0700

    update media/bytedance.py

--- media/bytedance.py
+++ media/bytedance.py
@@ -13,7 +13,7 @@ sa = ChatAction.UPLOAD_AUDIO
 
 class TikTokUser:
     def __init__(self, c, m, url, caption):
-        original = InlineKeyboardMarkup([[InlineKeyboardButton("TikTok User",url=url), InlineKeyboardButton("Group", url="https://t.me/contentdownload_group"),InlineKeyboardButton("Channel", url="https://t.me/contentdownload")]])
+        button = InlineKeyboardMarkup([[InlineKeyboardButton("TikTok User",url=url), InlineKeyboardButton("Group", url="https://t.me/contentdownload_group"),InlineKeyboardButton("Channel", url="https://t.me/contentdownload")]])
         media_links = get_media_links(url)
         list_video = []
         list_photo = []
@@ -30,11 +30,11 @@ class TikTokUser:
                 list_music.append(music)
         if list_photo:
             try:
-                send_photos(m, list_photo, original, caption)
+                send_photos(m, list_photo, button, caption)
             except Exception as e:
                 logging.error(e)
         if list_video:
-            send_videos(m, list_video, original, caption)
+            send_videos(m, list_video, button, caption)
         if list_music:
             for music in list_music:
                 m.reply_chat_action(sa)
@@ -53,13 +53,13 @@ def tikdou(m, attrs):
             TikTokUser(c, m, url, caption)
             return
         if is_video == False:
-            send_photos(m, file, original, caption)
+            send_photos(m, file, button, caption)
             if music:
                 m.reply_chat_action(sa)
                 m.reply_audio(music, caption=caption)
         elif is_video == True:
             m.reply_chat_action(sv)
-            m.reply_video(file, caption=caption, reply_markup=original)
+            m.reply_video(file, caption=caption, reply_markup=button)
         if m.chat.username == "contentdownload":
             upload(file)
         os.system("echo Completed")
