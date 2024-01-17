b5f1914e8e8eb233a7c0b22cfef2f0245c6951d8
mymaking <152869680+mymaking@users.noreply.github.com>
2024-01-17 13:02:08 +0700

    update activity/Server.py

--- activity/Server.py
+++ activity/Server.py
@@ -1,18 +1,16 @@
 from pyrogram import Client, filters
-from ext.util import server_info
+from ext import server_info
 import platform
 
 @Client.on_message(filters.command('server'))
 def bot_server_info(c, m):
-  ver = platform.version()
-  name = platform.uname()
-  system = platform.system()
-  server = server_info()
-  msg_text = (f"```{system}\n"
+    ver = platform.version()
+    name = platform.uname()
+    system = platform.system()
+    server = server_info()
+    msg_text = (f"```{system}\n"
               f"INFO:\n{name}\n\n"
               f"VERSION:\n{ver}\n\n"
               f"SERVER:\n{server}\n"
-              "```"
-                )
-
-  m.reply(msg_text)
+              "```")
+    m.reply(msg_text)
