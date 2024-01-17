f0399676b64354d4d2ab36399a62e50daf9fbdd4
mymaking <152869680+mymaking@users.noreply.github.com>
2024-01-17 12:05:14 +0700

    update media/meta.py

--- media/meta.py
+++ media/meta.py
@@ -1,6 +1,8 @@
 from api import FBDL, IGDL
 from ext import send_videos, send_photos
 from pyrogram.enums import ChatAction
+import logging
+import os
 
 rv = ChatAction.RECORD_VIDEO
 ra = ChatAction.RECORD_AUDIO
@@ -8,26 +10,38 @@ sv = ChatAction.UPLOAD_VIDEO
 sp = ChatAction.UPLOAD_PHOTO
 
 def facebook(m, attrs):
-    url = attrs.url
-    button = attrs.button
-    caption = attrs.caption
-    m.reply_chat_action(rv)
-    files = FBDL(url)
-    m.reply_chat_action(sv)
-    if not isinstance(files, list):
-        m.reply_video(files, caption=caption, reply_markup=button)
-    else:
-        for file in files:
-            m.reply_chat_action(sv)
-            m.reply_video(file, caption=caption, reply_markup=button)
+    try:
+        url = attrs.url
+        button = attrs.button
+        caption = attrs.caption
+        m.reply_chat_action(rv)
+        files = FBDL(url)
+        m.reply_chat_action(sv)
+        if not isinstance(files, list):
+            m.reply_video(files, caption=caption, reply_markup=button)
+        else:
+            for file in files:
+                m.reply_chat_action(sv)
+                m.reply_video(file, caption=caption, reply_markup=button)
+        os.system("echo Completed")
+        return
+    except Exception as e:
+        logging.error(e)
+        raise Exception(e)
 
 def instagram(m, attrs):
-    url = attrs.url
-    button = attrs.button
-    caption = attrs.caption
-    m.reply_chat_action(rv)
-    files, is_video = IGDL(url)
-    if is_video == False:
-        send_photos(m, files, button, caption)
-    elif is_video == True:
-        send_videos(m, files, button, caption)
+    try:
+        url = attrs.url
+        button = attrs.button
+        caption = attrs.caption
+        m.reply_chat_action(rv)
+        files, is_video = IGDL(url)
+        if is_video == False:
+            send_photos(m, files, button, caption)
+        elif is_video == True:
+            send_videos(m, files, button, caption)
+        os.system("echo Completed")
+        return
+    except Exception as e:
+        logging.error(e)
+        raise Exception(e)
