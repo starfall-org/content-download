48b05ea259ff0520ccb491efb2d371e0352ccab2
mymaking <152869680+mymaking@users.noreply.github.com>
2024-01-17 18:03:28 +0700

    update update.sh

--- update.sh
+++ update.sh
@@ -4,15 +4,15 @@ git config --global user.email "tonglanhchua@gmail.com"
 git config --global user.name "TLC"
 git add *
 GIT_COMMIT_MESSAGE="UPDATE"
-GIT_USERNAME=$GH_USERNAME
-GIT_PASSWORD=$GH_PASSWORD
+GIT_USERNAME="mymaking"
+GIT_PASSWORD=$GH_TOKEN
 git commit -a -m "$GIT_COMMIT_MESSAGE"
 
 expect -c "
 spawn git pull
-expect \"Username for 'https://huggingface.co': \"
+expect \"Username for 'https://github.com': \"
 send \"$GIT_USERNAME\r\"
-expect \"Password for 'https://$GIT_USERNAME@huggingface.co': \"
+expect \"Password for 'https://$GIT_USERNAME@github.com': \"
 send \"$GIT_PASSWORD\r\"
 interact
 "
