fc30ae3b93fdfa78ca9fa15ed49f05e44e79d274
mymaking <152869680+mymaking@users.noreply.github.com>
2024-01-17 15:03:27 +0700

    update ext/util.py

--- ext/util.py
+++ ext/util.py
@@ -29,8 +29,7 @@ def save(m):
 def send_photos(m, photo_links: List[str], button, caption):
     m.reply_chat_action(sp)
     if len(photo_links) == 1:
-        for link in photo_links:
-            m.reply_photo(link, reply_markup=button, caption=caption)
+        m.reply_photo(photo_links, reply_markup=button, caption=caption)
     else:
         for i in range(0, len(photo_links) - 1, 10):
             media_group = [InputMediaPhoto(link)
@@ -43,8 +42,7 @@ def send_photos(m, photo_links: List[str], button, caption):
 def send_videos(m, video_links: List[str], button, caption):
     m.reply_chat_action(sv)
     if len(video_links) == 1:
-        for link in video_links:
-            m.reply_video(link, reply_markup=button, caption=caption)
+        m.reply_video(video_links, reply_markup=button, caption=caption)
     else:
         for i in range(0, len(video_links) - 1, 10):
             media_group = [InputMediaVideo(link)
