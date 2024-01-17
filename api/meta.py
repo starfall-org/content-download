e7794e5d119882c29e87fd1354588d2ca176f407
mymaking <152869680+mymaking@users.noreply.github.com>
2024-01-17 15:19:54 +0700

    update api/meta.py

--- api/meta.py
+++ api/meta.py
@@ -2,33 +2,31 @@ import requests, random, os
 from io import BytesIO
 from init import dapi
 
-class FBDL:
-    def __init__(self, url):
-        data = requests.get(f"{dapi}/facebook", params={"url":url}, timeout=60).json()
-        files = []
-        for link in data["url"]:
-            content = requests.get(link).content
-            file = BytesIO(content)
-            file.name = f"file{random.randint(1, 9999)}.mp4"
-            files.append(file)
-        os.system("echo Facebook")
-        return files
+def FBDL(url):
+    data = requests.get(f"{dapi}/facebook", params={"url":url}, timeout=60).json()
+    files = []
+    for link in data["url"]:
+        content = requests.get(link).content
+        file = BytesIO(content)
+        file.name = f"file{random.randint(1, 9999)}.mp4"
+        files.append(file)
+    os.system("echo Facebook")
+    return files
 
 
 class IGDL:
-    def __init__(self, url):
-        data = requests.get(f"{dapi}/instagram", params={"url":url}, timeout=60).json()
-        files = []
-        if data.get("is_video", True):
-            is_video = True
-            for link in data["url"]:
-                content = requests.get(link).content
-                file = BytesIO(content)
-                file.name = f"instagram.mp4"
-                files.append(file)
-        else:
-            files = data["url"]
-            is_video = False
-            os.system("echo Instagram")
-        return files, is_video
-  
+def __init__(self, url):
+    data = requests.get(f"{dapi}/instagram", params={"url":url}, timeout=60).json()
+    files = []
+    if data.get("is_video", True):
+        is_video = True
+        for link in data["url"]:
+            content = requests.get(link).content
+            file = BytesIO(content)
+            file.name = f"instagram.mp4"
+            files.append(file)
+    else:
+        files = data["url"]
+        is_video = False
+        os.system("echo Instagram")
+    return files, is_video
