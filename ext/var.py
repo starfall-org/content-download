1de519fab1a455d27ea5f078f9521c07b39ba972
mymaking <152869680+mymaking@users.noreply.github.com>
2024-01-17 12:19:38 +0700

    update ext/var.py

--- ext/var.py
+++ ext/var.py
@@ -1,6 +1,4 @@
-from pyrogrecaudiom.types import InlineKeyboardMarkup, InlineKeyboardButton
-import webstream
-import re
+#
 #
 class Formats:
     def __init__(self):
@@ -19,26 +17,3 @@ class Formats:
         self.skip = [
            "application/json", "text/plain", "text/plain; charset=utf-8",
            "text/html; charset=UTF-8"]
-
-class BaseButton:
-    def __init__(self):
-        return InlineKeyboardMarkup([[
-            InlineKeyboardButton("Youtube",url="https://youtube.com/@DouyinShare")],[
-            InlineKeyboardButton("Group",url="https://t.me/contentdownload_group"),
-            InlineKeyboardButton("Channel", url="https://t.me/contentdownload")]])
-
-class Attrs:
-    def __init__(self, m):
-        try:
-            url = re.search(r"(?P<url>https?://[^\s]+)", m.text).group("url")
-        except:
-            url = re.search(r"(?P<url>https?://[^\s]+)", m.reply_to_message.text).group("url")
-        self.url = url
-        self.button = InlineKeyboardMarkup([[InlineKeyboardButton("Original", url=url), InlineKeyboardButton("Group", url="https://t.me/contentdownload_group"),InlineKeyboardButton("Channel", url="https://t.me/contentdownload")]])
-        try:
-            user_name = m.sender_chat.title
-            user_id = m.sender_chat.id
-        except:
-            user_name = m.from_user.first_name
-            user_id = m.from_user.id
-        self.caption = f'**Sent by --__[{user_name}](tg://user?id={user_id})__--**'
