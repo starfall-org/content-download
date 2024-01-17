ba2c408ad838e29cb3075697bdeb6207fe66df1f
mymaking <152869680+mymaking@users.noreply.github.com>
2024-01-17 11:38:39 +0700

    update media/meta.py

--- media/meta.py
+++ media/meta.py
@@ -2,13 +2,10 @@ from api import FBDL, IGDL
 from ext import Attrs, Actions,send_videos, send_photos
 from pyrogram.enums import ChatAction
 
-tp = ChatAction.TYPING
 rv = ChatAction.RECORD_VIDEO
 ra = ChatAction.RECORD_AUDIO
 sv = ChatAction.UPLOAD_VIDEO
 sp = ChatAction.UPLOAD_PHOTO
-sa = ChatAction.UPLOAD_AUDIO
-sd = ChatAction.UPLOAD_DOCUMENT
 
 def facebook(c, m):
     url = Attrs(m).url
@@ -18,10 +15,11 @@ def facebook(c, m):
     files = FBDL(url)
     m.reply_chat_action(sv)
     if not isinstance(files, list):
-        m.reply_is_video(files, caption=caption, reply_markup=button)
+        m.reply_video(files, caption=caption, reply_markup=button)
     else:
         for file in files:
-            m.reply_is_video(file, caption=caption, reply_markup=button)
+            m.reply_chat_action(sv)
+            m.reply_video(file, caption=caption, reply_markup=button)
 
 def instagram(c, m):
     url = Attrs(m).url
@@ -32,4 +30,4 @@ def instagram(c, m):
     if is_video == False:
         send_photos(m, files, button, caption)
     elif is_video == True:
-        send_is_videos(m, files, button, caption)
+        send_videos(m, files, button, caption)
