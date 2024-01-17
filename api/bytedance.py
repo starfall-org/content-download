b51110461bae97b96e92cfc5701b686ce8167f8a
mymaking <152869680+mymaking@users.noreply.github.com>
2024-01-17 09:21:02 +0700

    update api/bytedance.py

--- api/bytedance.py
+++ api/bytedance.py
@@ -2,25 +2,27 @@ import requests, logging
 from io import BytesIO
 from data.secret import dapi
 
-def TDDL(url):
-  data = requests.get(f"{dapi}/tikdou", params={"url": url}, timeout=60).json()
-  music = None
-  if data['is_video'] == True:
-    content = requests.get(data["url"]).content
-    file = BytesIO(content)
-    file.name = "video.mp4"
-    is_video = True
-  else:
-    file = []
-    for photo_url in data["url"]:
-        photo_data = requests.get(photo_url).content
-        photo_file = BytesIO(photo_data)
-        photo_file.name = "photo.jpg"
-        file.append(photo_file)
-    is_video = False
-    if data["music"]:
-        music_data = requests.get(data["music"]).content
-        music = BytesIO(music_data)
-        music.name = "music.mp3"
-  logging.critical("TikTok/Douyin")
-  return file, data["url"], is_video, music, data["music"]
+class TDDL(url):
+    def __init__(self, url):
+        data = requests.get(f"{dapi}/tikdou", params={"url": url}, timeout=60).json()
+    def get(self):
+        music = None
+        if data['is_video'] == True:
+            content = requests.get(data["url"]).content
+            file = BytesIO(content)
+            file.name = "tiktokdouyin.mp4"
+            is_video = True
+        else:
+            file = []
+            for photo_url in data["url"]:
+                photo_data = requests.get(photo_url).content
+                photo_file = BytesIO(photo_data)
+                photo_file.name = "photo.jpg"
+                file.append(photo_file)
+            is_video = False
+        if data["music"]:
+            music_data = requests.get(data["music"]).content
+            music = BytesIO(music_data)
+            music.name = "music.mp3"
+        os.system("echo TikTok/Douyin")
+        return file, data["url"], is_video, music, data["music"]
