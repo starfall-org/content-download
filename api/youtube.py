63c7bfa02d97642cc341eb36ec9f39b44e13f99c
mymaking <152869680+mymaking@users.noreply.github.com>
2024-01-17 15:21:19 +0700

    update api/youtube.py

--- api/youtube.py
+++ api/youtube.py
@@ -4,43 +4,40 @@ from io import BytesIO
 from ext import Formats
 
 
-class YTDL:
-    def __init__(self):
-        data = requests.get(f"{dapi}/youtube", params={"url":url}, timeout=60).json()
-        content = requests.get(data["url"]).content
-        file = BytesIO(content)
-        file.name = f"video.mp4"
-        return file
-        
-class YTM:
-    def __init__(self, url):
-        data = requests.get(f"{dapi}/music", params={"url":url}, timeout=60).json()
-        content = requests.get(data["url"]).content
-        file = BytesIO(content)
-        file.name = f"music.mp3"
-        return file
+def YTDL(url):
+    data = requests.get(f"{dapi}/youtube", params={"url":url}, timeout=60).json()
+    content = requests.get(data["url"]).content
+    file = BytesIO(content)
+    file.name = f"video.mp4"
+    return file
     
-class ODL:
-    def __init__(self, url):
-        data = requests.get(f"{dapi}/other", params={"url": url}, timeout=60).json()
-        r = requests.get(data["url"])
-        content = r.content
-        try:
-            header = r.headers['Content-Disposition']
-            filename = re.findall("filename=(.+)", header)[0]
-        except:
-            filename = f"{random.randint(1000,9000)}"
-            if r.headers["Content-Type"] in Formats().audio:
-                filename = f"{filename}.mp3"
-                type = "audio"
-                file = BytesIO(content)
-                file.name = filename
-            if r.headers["Content-Type"] in Formats().image:
-                type = "image"
-            elif r.headers["Content-Type"] in Formats().video:
-                type = "video"
-            if r.headers["Content-Type"] in Formats().skip:
-                return None, None
-            else:
-                type = "other"
-        return file, type
+def YTM(url):
+    data = requests.get(f"{dapi}/music", params={"url":url}, timeout=60).json()
+    content = requests.get(data["url"]).content
+    file = BytesIO(content)
+    file.name = f"music.mp3"
+    return file
+
+def ODL(url):
+    data = requests.get(f"{dapi}/other", params={"url": url}, timeout=60).json()
+    r = requests.get(data["url"])
+    content = r.content
+    try:
+        header = r.headers['Content-Disposition']
+        filename = re.findall("filename=(.+)", header)[0]
+    except:
+        filename = f"{random.randint(1000,9000)}"
+        if r.headers["Content-Type"] in Formats().audio:
+            filename = f"{filename}.mp3"
+            type = "audio"
+            file = BytesIO(content)
+            file.name = filename
+        if r.headers["Content-Type"] in Formats().image:
+            type = "image"
+        elif r.headers["Content-Type"] in Formats().video:
+            type = "video"
+        if r.headers["Content-Type"] in Formats().skip:
+            return None, None
+        else:
+            type = "other"
+    return file, type
