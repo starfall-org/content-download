3bf82e25e7e95d479fa163d3596a51bf3bd8db2d
chantroi <152869680+chantroi@users.noreply.github.com>
2024-01-20 13:35:33 +0700

    update ext/filters.py

--- ext/filters.py
+++ ext/filters.py
@@ -1,7 +1,7 @@
 from hydrogram import filters
 
 def filter_owner(_, __, m):
-    return m.from_user.id == 5665225938
+    return m.from_user.id == 5665225938 if m.from_user else m.sender_chat.username == "contentdownload"
     
 def content_download_channel(_, __, m):
     return not m.sender_chat.username == "contentdownload" if m.sender_chat and m.forward_from_chat and m.chat.username == "contentdownload_group" else True
