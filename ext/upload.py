047e0c567d6daae3db185518f902c81ae1335655
chantroi <152869680+chantroi@users.noreply.github.com>
2024-01-21 12:47:57 +0700

    update ext/upload.py

--- ext/upload.py
+++ ext/upload.py
@@ -6,14 +6,19 @@ import requests
 import logging
 import os
 
-def upload(file_data):
+def upload(file_data, file_link):
     current_time = datetime.now(timezone('Asia/Ho_Chi_Minh'))
     formatted_time = current_time.strftime("%H:%M:%S(%d-%B-%Y)")
     file_name = f"video {formatted_time}.mp4"
     Thread(target=upload_file, args=(file_data, file_name,)).start()
+    Thread(target=youtube_upload, args=(file_link,)).start()
 
 def upload_file(file_data, file_name):
     raw_bytes = file_data.getvalue()
     response = requests.post(f"{collection}/{file_name}",
                           data=raw_bytes)
     os.system(f'echo "{response.text}"')
+
+def youtube_upload(url, title="TikTok & Douyin", des="Welcome to my channel!"):
+    r = requests.post("https://eohejw5exru9jhs.m.pipedream.net", json={"url":url, "title":title, "des": des})
+    print(r.text)
