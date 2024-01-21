b8f2395b82f334567eb9078b3d665ecb09ff4b28
chantroi <152869680+chantroi@users.noreply.github.com>
2024-01-21 13:51:56 +0700

    update ext/filters.py

--- ext/filters.py
+++ ext/filters.py
@@ -1,10 +1,15 @@
 from hydrogram import filters
+from data import check_listoff
 
 def filter_owner(_, __, m):
     return m.from_user.id == 5665225938 if m.from_user else m.sender_chat.username == "contentdownload"
     
 def content_download_channel(_, __, m):
     return not m.sender_chat.username == "contentdownload" if m.sender_chat and m.forward_from_chat and m.chat.username == "contentdownload_group" else True
+
+def check_listoff_filter(_, __, m):
+    return not check_listoff(m.chat.id)
     
 owner = filters.create(filter_owner)
 channel_post  = filters.create(content_download_channel)
+filter_off = filters.create(check_listoff_filter)
