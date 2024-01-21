23cd3bf8a095847f714c425e160c5192e6493626
chantroi <152869680+chantroi@users.noreply.github.com>
2024-01-21 14:27:33 +0700

    update data/off_download.py

--- data/off_download.py
+++ data/off_download.py
@@ -1,24 +1,32 @@
 
 def add_off(chat_id):
-    with open("listoff.txt", "r") as f:
-        chat_ids = f.read().splitlines()
-        if str(chat_id) not in chat_ids:
-            with open("listoff.txt", "a") as w:
-                w.write("\n" + str(chat_id))
-            return True
-        else:
-            return False
-                
-def rm_off(chat_id):
-    with open("listoff.txt", "r") as f:
-        chat_ids = f.read().splitlines()
-        if str(chat_id) in chat_ids:
-            chat_ids.remove(str(chat_id)) 
-            with open("listoff.txt", "w") as w:
-                w.write("\n".join(chat_ids))
+    if os.path.exits("listoff.txt"):
+        with open("listoff.txt", "r") as f:
+            chat_ids = f.read().splitlines()
+            if str(chat_id) not in chat_ids:
+                with open("listoff.txt", "a") as w:
+                    w.write("\n" + str(chat_id))
                 return True
-        else:
-            return False
+            else:
+                return False
+    else:
+        with open("listoff.txt", "a") as w:
+                w.write("\n" + str(chat_id))
+                return True
+        
+def rm_off(chat_id):
+    if os.path.exits("listoff.txt"):
+        with open("listoff.txt", "r") as f:
+            chat_ids = f.read().splitlines()
+            if str(chat_id) in chat_ids:
+                chat_ids.remove(str(chat_id)) 
+                with open("listoff.txt", "w") as w:
+                    w.write("\n".join(chat_ids))
+                    return True
+            else:
+                return False
+    else:
+        return False
                 
 def check_listoff(chat_id):
     with open("listoff.txt", "r") as f:
