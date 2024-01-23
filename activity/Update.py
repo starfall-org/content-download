ae9961b1dfe92faa6f7017913c8a2e6dda6153a0
chantroi <152869680+chantroi@users.noreply.github.com>
2024-01-23 11:28:32 +0700

    update activity/Update.py

--- activity/Update.py
+++ activity/Update.py
@@ -10,7 +10,7 @@ typing = ChatAction.TYPING
 @Client.on_message(filters.command("update") & filters.private & owner)
 def system_update(c, m):
     m.reply_chat_action(typing)
-    result = subprocess.run(["bash", "update.sh"], stdout=subprocess.PIPE, text=True, shell=True)
+    result = subprocess.run(["bash", "update.sh"], stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True, shell=True)
     m.reply(f"```bash\n{result.stdout}\n```")
     os.execl(sys.executable, sys.executable, *sys.argv)
     
@@ -18,5 +18,5 @@ def system_update(c, m):
 def run_shell(c, m):
     m.reply_chat_action(typing)
     command = m.text.replace("/shell ", "")
-    result = subprocess.run(command, stdout=subprocess.PIPE, text=True, shell=True)
+    result = subprocess.run(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True, shell=True)
     m.reply(f"```bash\n{result.stdout}\n```")
