6da92056a95fe7f6db361462fbb672125e3e1389
chantroi <152869680+chantroi@users.noreply.github.com>
2024-01-26 09:42:06 +0700

    update activity/SetProxies.py

--- activity/SetProxies.py
+++ activity/SetProxies.py
@@ -5,6 +5,6 @@ import subprocess
 @Client.on_message(filters.command("proxy") & owner)
 def set_proxy(c, m):
     proxy = m.command[1]
-    subprocess.run(["./lite", proxy], shell=True)
+    subprocess.run(["./lite", proxy, "&"], shell=True)
     m.reply("Đã thiết lập proxy thành công")
     m.delete()
