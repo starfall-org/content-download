b6e0f1a55a02dcb5ed4adb6588794de65a32aa20
chantroi <152869680+chantroi@users.noreply.github.com>
2024-01-26 19:36:20 +0700

    update api/youtube.py

--- api/youtube.py
+++ api/youtube.py
@@ -1,19 +1,19 @@
 import requests, random
 from init import dapi
 from io import BytesIO
-from ext import Formats, request_get
+from ext import Formats
 
 
 def YTDL(url):
     data = requests.get(f"{dapi}/youtube", params={"url":url}, timeout=60).json()
-    content = request_get(data["url"])
+    content = requests.get(data["url"]).content
     file = BytesIO(content)
     file.name = f"video.mp4"
     return file
     
 def YTM(url):
     data = requests.get(f"{dapi}/music", params={"url":url}, timeout=60).json()
-    content = request_get(data["url"])
+    content = requests.get(data["url"]).content
     file = BytesIO(content)
     file.name = f"music.mp3"
     return file
