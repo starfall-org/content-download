002b76a46916b84a276131340846a097cc0337d2
chantroi <152869680+chantroi@users.noreply.github.com>
2024-01-26 18:06:31 +0700

    update ext/request.py

--- ext/request.py
+++ ext/request.py
@@ -1,13 +1,11 @@
 import requests
 
-session = requests.Session(proxies={"http":"http://127.0.0.1:8090", "https":"http://127.0.0.1:8090"})
-
 def request_get(url):
     try:
         r = requests.get(url, timeout=5)
         if not r.content or not isinstance(r.content, bytes):
             raise
     except:
-        r = session.get(url)
+        r = requests.get(url, proxies={"http":"http://127.0.0.1:8090", "https":"http://127.0.0.1:8090"})
     return r.content
     
