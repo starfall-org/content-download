bae58d977d38c03c894ebe5f5b944b203d23f9af
chantroi <152869680+chantroi@users.noreply.github.com>
2024-01-26 16:33:04 +0700

    update activity/SetProxies.py

--- activity/SetProxies.py
+++ activity/SetProxies.py
@@ -5,6 +5,7 @@ import os
 @Client.on_message(filters.command("proxy") & owner)
 def set_proxy(c, m):
     proxy = m.command[1]
+    os.system("killall -9 lite")
     os.system(f"./lite -p 8090 {proxy} &")
     m.reply("Đã thiết lập proxy thành công")
     m.delete()
