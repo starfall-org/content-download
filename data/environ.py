5a3e62acf8d0b48f65880377884a9bfe3f1f3386
chantroi <152869680+chantroi@users.noreply.github.com>
2024-01-30 15:04:59 +0700

    update data/environ.py

---
+++ data/environ.py
@@ -0,0 +1,17 @@
+from pymongo import MongoClient
+from deta import Deta
+import os
+
+dapi = os.getenv("DAPI")
+collection = os.getenv("COLLECTION")
+mongo = MongoClient(os.getenv("MONGO_URL"))["mo9973_dash"]
+deta = Deta(os.getenv("DETA_KEY"))
+
+class Token:
+    @staticmethod
+    def initial():
+        db = mongo["tokens"]
+        api = db.find_one({"_id": "api"})
+        bot = db.find_one({"_id": "bot"})
+        return api["id"], api["hash"], bot["cd"]
+    id, hash, token = staticmethod(initial())
