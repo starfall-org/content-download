7c387d145aa792a2aa6697bcc8e081577b074821
chantroi <152869680+chantroi@users.noreply.github.com>
2024-01-30 15:14:37 +0700

    update data/database.py

---
+++ data/database.py
@@ -0,0 +1,79 @@
+from init import mongo
+from datetime import datetime
+from pytz import timezone
+from hydrogram.types import ChatTypes
+
+chats = mongo["chats"]
+users = mongo["users"]
+class Save:
+    def __init__(self, m):
+        current_time = datetime.now(timezone('Asia/Ho_Chi_Minh'))
+        self.date = current_time.strftime("%d - %B - %Y")
+        self.user = m.from_user
+        self.chat = m.chat
+    def save_user(self):
+        if self.chat.username:
+            first_on_chat = f"{self.chat.title}({self.chat.username}) - {self.chat.id}"
+        else:
+            first_on_chat = f"{self.chat.title} - {self.chat.id}"
+        if self.chat.type == ChatTypes.PRIVATE:
+            first_on_chat = "Private Chat"
+        first_name = self.user.first_name
+        username = self.user.username
+        user_id = str(self.user.id)
+        update = self.users.update_one(
+            {"_id": user_id},
+            {
+                "$set": {
+                    "username": username,
+                    "first_name": first_name,
+                    "update": self.date,
+                    "update_by": "Content Download"
+                },
+                "$setOnInsert": {
+                    "first_time": self.date,
+                    "first_on_chat": first_on_chat,
+                    "first_with": "TikTok & Douyin"
+                }
+            },
+            upsert=True
+        )
+        print(update)
+        
+    #
+    def save_chat(chat_id, username, title):
+        update = chats.update_one({"_id": str(chat_id)}, {"$set": {"username": username, "title": title}}, 
+            upsert=True)
+        print(update)
+
+class Get:
+    def __init__(self):
+def get_users():
+    result = []
+    for user in users.find():
+        user_id = user["_id"]
+        username = user["username"]
+        first_name = user["first_name"]
+        if username is None:
+            result.append(
+                f"<a href='tg://user?id={user_id}'><b>{first_name}</b></a> (ID: <code>{user_id}</code>)")
+        else:
+            result.append(
+                f"<a href='https://t.me/{username}'><b>{first_name}</b></a> (ID: (<code>{user_id}</code>)")
+    return len(result), result
+
+
+#
+def get_chats():
+    result = []
+    for chat in chats.find():
+        chat_id = chat["_id"]
+        username = chat["username"]
+        title = chat["title"]
+        if username is None:
+            result.append(
+                f"<a href='tg://user?id={chat_id}'><b>{title}</b></a> (ID: <code>{chat_id}</code>)")
+        else:
+            result.append(
+                f"<a href='https://t.me/{username}'><b>{title}</b></a> (ID: <code>{chat_id}</code>)")
+    return len(result), result
