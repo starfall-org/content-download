845a13d054ab1f830202ea5124dab62d18d54aae
chantroi <tonglanhchua@gmail.com>
2024-02-02 16:30:44 +0700

    update data/database.py

--- data/database.py
+++ data/database.py
@@ -52,6 +52,7 @@ class Save:
                 update_time = EXCLUDED.update_time,
                 update_by = EXCLUDED.update_by
         """, (chat_id, username, title, date, "Content Download", date, "Content Download"))
+        
         pg.commit()
 
 class Get:
