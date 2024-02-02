82fcbf34d45657cb26f5842a00edf2ff782360b8
chantroi <tonglanhchua@gmail.com>
2024-02-02 15:16:50 +0700

    update start.sh

--- start.sh
+++ start.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/bash
 
+curl --create-dirs -o $HOME/.postgresql/root.crt 'https://cockroachlabs.cloud/clusters/7616fc22-317d-43cf-bf87-4a51d4f339b6/cert'
+
 chmod +x ./main.py ./lite
 
 gunicorn --log-level critical -b 0.0.0.0:8080 application:app &
