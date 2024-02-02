91aa7bce98883caa7370bc7e9c3b1ee7245eec74
chantroi <tonglanhchua@gmail.com>
2024-02-02 14:30:40 +0700

    update data/environ.py

--- data/environ.py
+++ data/environ.py
@@ -3,13 +3,14 @@ from deta import Deta
 import psycopg2, requests
 import json, os
 
-secret = requests.get(os.getenv("SECRET")).text
-res = json.loads(secret, object_hook=lambda _: SimpleNamespace(**_))
+def res():
+    secret = requests.get(os.getenv("SECRET")).text
+    return json.loads(secret, object_hook=lambda _: SimpleNamespace(**_))
 
-conn = psycopg2.connect(res.data.cr_pg)
+conn = psycopg2.connect(res().data.cr_pg)
 cursor = conn.cursor()
 
 class Token:
-    id = res.key.api_id
-    hash = res.key.api_hash
-    token = res.bot.cd_tg
+    id = res().key.api_id
+    hash = res().key.api_hash
+    token = res().bot.cd_tg
