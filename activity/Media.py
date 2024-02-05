a022a261dbccffe8c35219d5a418d2d3cefa4904
chantroi <tonglanhchua@gmail.com>
2024-02-05 18:56:26 +0700

    update activity/Media.py

--- activity/Media.py
+++ activity/Media.py
@@ -21,7 +21,7 @@ def music_download(c, m):
             raise Exception(e)
     except Exception as e:
         m.reply_chat_action(typing)
-        m.reply("__--**INACCESSIBLE RESOURCES**--__", quote=True)
+        m.reply("__--**resource unavailable**--__", quote=True)
         logging.critical(e)
 
 @Client.on_message(((filters.regex("http://|https://") & filter_on) | filters.command('download')) & filters.incoming & channel_post)
@@ -44,7 +44,7 @@ def all_media_download(c, m):
                 m.delete()
             except Exception as e:
                 m.reply_chat_action(typing)
-                m.reply("__--**INACCESSIBLE RESOURCES**--__", quote=True)
+                m.reply("__--**resource unavailable**--__", quote=True)
                 logging.critical(e)
         else:
             try:
