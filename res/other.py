221adf6c330c5dd66e36f3f8575186422c0001f7
chantroi <tonglanhchua@gmail.com>
2024-02-07 16:13:13 +0700

    update res/other.py

---
+++ res/other.py
@@ -0,0 +1,52 @@
+from hydrogram.enums import ChatAction
+from data import dapi
+from io import BytesIO
+from ext import Formats
+import re, logging, requests
+
+
+def ODL(url):
+    data = requests.get(f"{dapi}/other", params={"url": url}, timeout=60).json()
+    r = requests.get(data["url"])
+    content = r.content
+    try:
+        header = r.headers['Content-Disposition']
+        filename = re.findall("filename=(.+)", header)[0]
+    except Exception:
+        filename = "download"
+        if r.headers["Content-Type"] in Formats().audio:
+            filename = f"{filename}.mp3"
+            types = "audio"
+            file = BytesIO(content)
+            file.name = filename
+        if r.headers["Content-Type"] in Formats().image:
+            types = "image"
+        elif r.headers["Content-Type"] in Formats().video:
+            types = "video"
+        if r.headers["Content-Type"] in Formats().skip:
+            return None, None
+        else:
+            types = "other"
+    return file, types
+
+def other(m, attrs):
+    try:
+        url = attrs.url
+        button = attrs.button
+        caption = attrs.caption
+        file, types = ODL(url)
+        if types == "image":
+            m.reply_chat_action(ChatAction.UPLOAD_PHOTO)
+            m.reply_photo(file, caption=caption, reply_markup=button)
+        elif types == "video":
+            m.reply_chat_action(ChatAction.UPLOA_VIDEO)
+            m.reply_video(file, caption=caption, reply_markup=button)
+        elif types == "audio":
+            m.reply_chat_action(ChatAction.UPLOAD_AUDIO)
+            m.reply_audio(file, caption=caption)
+        else:
+            m.reply_chat_action(ChatAction.UPLOAD_DOCUMENT)
+            m.reply_document(file, reply_markup=button, caption=caption)
+        print("Completed")
+    except Exception as e:
+        logging.error(e)
