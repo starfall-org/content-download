6bd99a39e870c16e226b9f4a22a0f95e617fb5be
chantroi <tonglanhchua@gmail.com>
2024-02-07 16:26:26 +0700

    update res/__init__.py

---
+++ res/__init__.py
@@ -0,0 +1,6 @@
+from .bytedance import tiktokdouyin
+from .youtube import youtube
+from .facebook import facebook
+from .instagram import instagram
+from .music import music
+from .other import other
