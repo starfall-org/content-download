dc3e264eafeceb18f5f70ca796dc1d399c0a6900
chantroi <tonglanhchua@gmail.com>
2024-02-07 16:14:54 +0700

    update res/music.py

---
+++ res/music.py
@@ -0,0 +1,24 @@
+from hydrogram.enums import ChatAction
+from data import dapi
+from io import BytesIO
+from ext import Formats
+import logging, requests
+
+def YTM(url):
+    data = requests.get(f"{dapi}/music", params={"url":url}, timeout=60).json()
+    content = requests.get(data["url"]).content
+    file = BytesIO(content)
+    file.name = "music.mp3"
+    return file
+
+def music(m, attrs):
+    try:
+        url = attrs.url
+        caption = attrs.caption
+        m.reply_chat_action(ChatAction.RECORD_AUDIO)
+        audio = YTM(url)
+        m.reply_chat_action(ChatAction.UPLOAD_AUDIO)
+        m.reply_audio(audio, caption=caption)
+        print("Completed")
+    except Exception as e:
+        logging.error(e)
