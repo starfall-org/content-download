61a03700beb05b23478d779e9de3d23e9a22ccd2
chantroi <duongchantroi@alwaysdata.net>
2024-06-04 11:56:36 +0000

    update PythonImportHelper-v2-Completion.json and environment.py

--- data/environment.py
+++ data/environment.py
@@ -3,11 +3,11 @@ import os
 from types import SimpleNamespace
 import requests
 
-secret = requests.get(os.getenv("SECRET")).text
+secret = requests.get(os.getenv("SECRET"), timeout=99).text
 res = json.loads(secret, object_hook=lambda _: SimpleNamespace(**_))
 
-WEB_COLLECTION = res.key.web_collection
+WEB_COLLECTION = res.deta.web_collection
 DAPI = res.api.dapi
 COLLECTON = res.api.collection
-DATABASE_URL = res.data.cockroach
-bot_token = res.bot.cd_tg
+DATABASE_URL = res.postgres.cockroach
+bot_token = res.bot.contentdownload
