fc4bcc1f03a83a076d51742cc5f6ddb49757df17
chantroi <duongchantroi@alwaysdata.net>
2024-06-11 16:36:11 +0000

    update PythonImportHelper-v2-Completion.json and environment.py

--- data/environment.py
+++ data/environment.py
@@ -6,8 +6,8 @@ import requests
 secret = requests.get(os.getenv("SECRET"), timeout=99).text
 res = json.loads(secret, object_hook=lambda _: SimpleNamespace(**_))
 
-WEB_COLLECTION = res.deta.web_collection
+WEB_COLLECTION = ""
 DAPI = res.api.dapi
-COLLECTON = res.api.collection
-DATABASE_URL = res.postgres.cockroach
+COLLECTON = ""
+DATABASE_URL = res.database.contentdownload
 bot_token = res.bot.contentdownload
