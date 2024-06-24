f96eaf83ecc22dc041bab8b8f27de9600702ca95
chantroi <duongchantroi@alwaysdata.net>
2024-06-24 01:21:05 +0000

    update PythonImportHelper-v2-Completion.json and environment.py

--- data/environment.py
+++ data/environment.py
@@ -4,5 +4,5 @@ import requests
 res = requests.get(os.getenv("SECRET"), timeout=99).json()
 
 DAPI = res["api"]["dapi"]
-DATABASE_URL = res["database"]["contentdownload"]
+DATABASE_URL = res["postgres"]["cockroach"]
 bot_token = res["bot"]["contentdownload"]
