f5a50f7dd4ec4b7bf6d2f6d0ba5ff5c6ce65f88a
AzureVM <tonglanhchua@gmail.com>
2024-07-23 15:07:48 +0700

    update environment.py

--- data/environment.py
+++ data/environment.py
@@ -4,10 +4,10 @@ import requests
 res = requests.get(os.getenv("SECRET"), timeout=99).json()
 
 dapi = res["api"]["dapi"]
-DATABASE_URL = res["postgres"]["cockroach"]
-bot_token = res["bot"]["contentdownload"]
-DAPI_YT = dapi['yt']
-DAPI_YTM = dapi['ytm']
-DAPI_FB = dapi['fb']
-DAPI_IG = dapi['ig']
-DAPI_TD = dapi['td']
+DATABASE_URL = res["db"]["postgres"][0]
+bot_token = res["access"]["telegram"]["cd"]
+DAPI_YT = dapi["yt"]
+DAPI_YTM = dapi["ytm"]
+DAPI_FB = dapi["fb"]
+DAPI_IG = dapi["ig"]
+DAPI_TD = dapi["td"]
