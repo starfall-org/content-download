b7014792972d1cb2c480f43c8a414a01cab646eb
CodeSandbox <tonglanhchua@gmail.com>
2025-01-30 06:13:31 +0000

    Add support for music and audio commands in YouTube handler

--- plugins/downloads.py
+++ plugins/downloads.py
@@ -19,11 +19,20 @@ def __print__(m: Message, platform: str):
 
 @Client.on_message(filters.regex("http|https") & filters.regex("youtube.|youtu.be"))
 async def __youtube__(_: Client, m: Message):
-
     await m.reply_chat_action(ChatAction.TYPING)
-    result = await api_handler("youtube", m)
-    await send_media(m, result.result, result.button, result.caption)
-    __print__(m, "youtube")
+    if any(command.upper() in ["MUSIC", "AUDIO"] for command in m.command):
+        result = await api_handler("music", m)
+        await m.reply_chat_action(ChatAction.UPLOAD_AUDIO)
+        await m.reply_audio(
+            result.result.content.url,
+            caption=result.caption,
+            reply_markup=result.button,
+        )
+        __print__(m, "music")
+    else:
+        result = await api_handler("youtube", m)
+        await send_media(m, result.result, result.button, result.caption)
+        __print__(m, "youtube")
     await save(m)
     await m.delete()
 
