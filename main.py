aec9f8bf0a6df23cfdf0bfda67b10f618c7eb494
CodeSandbox <tonglanhchua@gmail.com>
2025-02-27 06:15:10 +0000

    Add .env and client.session to .gitignore; install uvloop event loop conditionally

--- main.py
+++ main.py
@@ -1,19 +1,34 @@
-import uvloop
-from gevent import monkey
-
-uvloop.install()
-monkey.patch_all()
 import os
 
+import uvloop
 from dotenv import load_dotenv
 
 from bg_task import scheduled
 from client import make_bot, serve
+import asyncio
 
 load_dotenv()
+uvloop.install()
+
+
+def install_uvloop_event_loop():
+    if "uvloop" == asyncio.get_event_loop_policy().__module__:
+        return
+
+    try:
+        import uvloop
+
+        uvloop.install()
+    except ImportError as e:
+        print(f"Failed to install uvloop: {e}")
+    policy = asyncio.get_event_loop_policy()
+    print(f"Current event loop policy: {policy.__class__.__name__}")
+
+
 app = make_bot(
     os.environ["BOT_TOKEN"], os.environ["CONTENT_API"], os.environ["DATABASE_URL"]
 )
 
 if __name__ == "__main__":
+    install_uvloop_event_loop()
     app.run(serve(app, scheduled))
