9e250cc607ee9babca19487fbd5122a9f055d956
Contributed Repository <bosuutap@alwaysdata.net>
2025-12-08 14:17:35 +0000

    chore: remove test script and add backtick to system usage output string.

--- bot/utils/sys_usage.py
+++ bot/utils/sys_usage.py
@@ -7,4 +7,4 @@ process = psutil.Process(os.getpid())
 def show_usage(tag=""):
     mem = process.memory_info().rss / 1024 / 1024
     cpu = process.cpu_percent(interval=None)
-    return f"[{tag}] CPU: {cpu:5.1f}% | RAM: {mem:7.2f} MB"
+    return f"`[{tag}] CPU: {cpu:5.1f}% | RAM: {mem:7.2f} MB"
--- test.py
+++
@@ -1,5 +0,0 @@
-from bot.services.google_genai.core import GoogleGenAI
-
-gg = GoogleGenAI()
-for model in gg.models:
-    print(model.name)
